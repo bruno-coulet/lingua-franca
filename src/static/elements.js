c4ff73b80842f2f10b35b023e54559e96e4167ea
Christian Aucane <christianaucane1@gmail.com>
2024-05-14 10:24:42 +0200

    Put DOM elements in elements.js

---
+++ src/static/elements.js
@@ -0,0 +1,4 @@
+const translationForm = document.getElementById('translation-form');
+const result = document.getElementById('result');
+const resultMessagesWrapper = document.getElementById('result-messages-wrapper');
+
