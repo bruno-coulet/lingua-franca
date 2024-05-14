636124ff4987714694567d12fd3f6b581883a9d8
Christian Aucane <christianaucane1@gmail.com>
2024-05-14 12:10:12 +0200

    Add validators

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField, TextAreaField
+from wtforms import SelectField, TextAreaField
+from wtforms.validators import DataRequired
 
 from translation import list_languages
 
@@ -9,14 +10,17 @@ class TranslationForm(FlaskForm):
     source_language = SelectField("Source language",
                                   choices=[],
                                   default="auto",
-                                  render_kw={"id": "source-language"})
+                                  render_kw={"id": "source_language"},
+                                  validators=[DataRequired("Please select a source language")])
     target_language = SelectField("Target language",
                                   choices=[],
                                   default="auto",
-                                  render_kw={"id": "target-language"})
+                                  render_kw={"id": "target_language"},
+                                  validators=[DataRequired("Please select a target language")])
     text_to_translate = TextAreaField("Text",
-                                    render_kw={"id": "text-to-translate",
-                                               "placeholder": "Enter text to translate ..."})
+                                      render_kw={"id": "text_to_translate",
+                                              "placeholder": "Enter text to translate ..."},
+                                      validators=[DataRequired("Please enter text to translate")])
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
