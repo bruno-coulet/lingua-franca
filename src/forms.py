9148911093f15cd035d2e5277762bc586ab9ffa6
Christian Aucane <christianaucane1@gmail.com>
2024-05-22 19:28:14 +0200

    Create FileUploadForm

--- src/forms.py
+++ src/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from markupsafe import Markup
-from wtforms import SelectField, TextAreaField
+from wtforms import FileField, SelectField, TextAreaField
 from wtforms.validators import DataRequired
 
 from translation import list_languages
 
+AVAILABLE_LANGUAGES = list_languages()
 
 class TranslationForm(FlaskForm):
     source_language = SelectField("Source language",
@@ -30,6 +31,21 @@ class TranslationForm(FlaskForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        available_languages = list_languages()
-        self.source_language.choices = [("auto", "Automatic Detection")] + available_languages
-        self.target_language.choices = available_languages
+        self.source_language.choices = [("auto", "Automatic Detection")] + AVAILABLE_LANGUAGES
+        self.target_language.choices = AVAILABLE_LANGUAGES
+
+
+class FileUploadForm(FlaskForm):
+    file = FileField("File to translate",
+                     id="file-to-translate",
+                     validators=[DataRequired("Please select a file to translate"),
+                                 ])
+    target_language = SelectField("Target language",
+                                  id="target-language",
+                                  choices=[],
+                                  default="",
+                                  validators=[DataRequired("Please select a target language")])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.target_language.choices = [("", "Select a target language")] + AVAILABLE_LANGUAGES
